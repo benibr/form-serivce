419ca6c4cbf1e6d5ccf5bd9fe0523354e13657b7
Benedikt Braunger <bebr@adm.ku.dk>
2024-05-24 12:16:50 +0200

    added env vars+debug output

--- main.go
+++ main.go
@@ -2,17 +2,35 @@ package main
 
 import (
   "fmt"
+  "strconv"
+  "os"
   "io"
   "net/http"
 )
 
+// globals
+var debug bool = false
+
+func debugFormData(r *http.Request) {
+  fmt.Print("DEBUG: Full form object:")
+  fmt.Printf("  %v\n\n", r)
+}
+
+func getEnvConfig() {
+  debug, _ = strconv.ParseBool(os.Getenv("FORM_SERVICE_DEBUG"))
+}
+
 func getSubmission(w http.ResponseWriter, r *http.Request) {
   fmt.Println("Received form submission\n")
+  if debug {
+    debugFormData(r)
+  }
   io.WriteString(w, "Form submitted, I promise!!\n")
 }
 
 func main() {
-  http.HandleFunc("/submit", getSubmission)
+  getEnvConfig()
 
+  http.HandleFunc("/submit", getSubmission)
   http.ListenAndServe(":3333", nil)
 }
